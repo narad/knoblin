1b80dc1ae3a19a0b4807307c73d6aff770483348
narad <narad@hilbert.local>
2021-11-14 19:15:52 +0900

    added comments to original servo controller ino file

--- arduino/servo_controller/servo_controller.ino
+++ arduino/servo_controller/servo_controller.ino
@@ -1,20 +1,14 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// From the original driver demo:
-// called this way, it uses the default address 0x40
+// This script is design for controlling any servo hooked up
+// to a PCA9685 servo control unit via Arduino.  It controls
+// one servo at a time.  To use it, use the Python serial.Serial
+// package to send a byte message of the form "servo_id:position", 
+// i.e. the String "1:350" moves the first servo to 350 (center, usually)
+
+
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-// #define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-// #define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
-
-
 String inByte;  // byte read from port, can come from Python script
 String c_pos;   // read byte encoding of servo position
 String c_servo; // read byte encoding target servo
@@ -26,35 +20,41 @@ int delay_time = 0; // delay between servo move increments
 
 
 void setup() {
-  Serial.setTimeout(250); // drastically reduce Serial.readStringUntil time
+  // drastically reduce Serial.readStringUntil time
+  Serial.setTimeout(250); 
+  // initiate serial protocol
   Serial.begin(9600);
   Serial.println("Initializing.");
+  // initiate servos
   pwm.begin();
-  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+  // Analog servos run at ~60 Hz updates
+  pwm.setPWMFreq(60); 
+  // set an initial 'last position'.  Once the servo is moved
+  // and this updates, the servo can be moved incrementally
   last = -1;
 }
 
 
 void loop() {    
-  if(Serial.available()) { // if data available in serial port
-    // read data until newline
-    inByte = Serial.readStringUntil('\n'); 
-    Serial.println(inByte);
+  if(Serial.available()) {
+    message = Serial.readStringUntil('\n'); 
+    Serial.println(message);
 
     // Unpack servo and position ID from byte sent on comm port
-    c_servo = inByte.substring(0, 1);
-    c_pos = inByte.substring(2, inByte.length());
+    c_servo = message.substring(0, 1);
+    c_pos = message.substring(2, message.length());
 
     // Convert to int
     servo = c_servo.toInt();
     pos = c_pos.toInt();  
-//    delay_time = c_delay.toInt(); local delay time deprecated
 
     // Move the servo
+    // Moving without delay
     if (last == -1 || delay_time == 0) {
       pwm.setPWM(servo, 0, pos);
       last = pos;
     }
+    // Moving incrementally with delay
     else if (last < pos) {
       while (last < pos) {
         pwm.setPWM(servo, 0, last);
